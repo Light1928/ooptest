7123ed36fa313b94be5efdd877b0ad15609747c1
Light <51193957+Light1928@users.noreply.github.com>
2019-07-07 08:05:01 +0900

    ページ遷移実装

--- src/ooptest/SalaPay.java
+++ src/ooptest/SalaPay.java
@@ -2,7 +2,7 @@ package ooptest;
 
 public class SalaPay {
 	public static void main(String[] args) {
-		Log_in login = new Log_in("ログイン");
+		Log_in login = new Log_in("SalaPay");
 		login.panelSet();
 		login.setVisible(true);
 	}
