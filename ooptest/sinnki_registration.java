943607f9e173d6bd676058bd97fcab86ec4453fe
Light <51193957+Light1928@users.noreply.github.com>
2019-07-09 10:52:36 +0900

    新規登録作成

--- ooptest/sinnki_registration.java
+++ ooptest/sinnki_registration.java
@@ -1,5 +1,112 @@
 package ooptest;
 
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import java.awt.Font;
+import java.awt.TextField;
+import java.awt.Button;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+import javax.swing.Action;
+import javax.swing.JButton;
+
 public class sinnki_registration extends JFrame {
 
+	private JPanel contentPane;
+
+
+	/**
+	 * Create the frame.
+	 */
+	public sinnki_registration() {
+		setSize(900,600);
+		setLocationRelativeTo(null);
+		setResizable(false);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		setBounds(100, 100, 900, 600);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel label = new JLabel("新規登録");
+		label.setFont(new Font("MS UI Gothic", Font.BOLD, 55));
+		label.setBounds(295, 53, 232, 99);
+		contentPane.add(label);
+		
+		JLabel lblid = new JLabel("ユーザID");
+		lblid.setFont(new Font("MS UI Gothic", Font.BOLD, 20));
+		lblid.setBounds(191, 187, 101, 34);
+		contentPane.add(lblid);
+		
+		JLabel label_1 = new JLabel("パスワード");
+		label_1.setFont(new Font("MS UI Gothic", Font.BOLD, 20));
+		label_1.setBounds(191, 275, 101, 34);
+		contentPane.add(label_1);
+		
+		JLabel label_2 = new JLabel("名前");
+		label_2.setFont(new Font("MS UI Gothic", Font.BOLD, 20));
+		label_2.setBounds(213, 362, 59, 34);
+		contentPane.add(label_2);
+		
+		TextField textField = new TextField();
+		textField.setBounds(298, 187, 333, 34);
+		contentPane.add(textField);
+		
+		TextField textField_1 = new TextField();
+		textField_1.setBounds(298, 275, 333, 34);
+		contentPane.add(textField_1);
+		
+		TextField textField_2 = new TextField();
+		textField_2.setBounds(298, 362, 333, 34);
+		contentPane.add(textField_2);
+		
+		Button button = new Button("登録");
+		button.setFont(new Font("Dialog", Font.BOLD, 17));
+		button.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		button.setBounds(361, 436, 87, 25);
+		contentPane.add(button);
+		
+		JButton button_1 = new JButton("新規登録");
+		button_1.setFont(new Font("MS UI Gothic", Font.PLAIN, 15));
+		button_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		button_1.setBounds(629, 0, 113, 19);
+		contentPane.add(button_1);
+		
+		JButton button_2 = new JButton("アカウント削除");
+		button_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		button_2.setBounds(754, 0, 128, 19);
+		contentPane.add(button_2);
+		
+		JButton button_3 = new JButton("月選択");
+		button_3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		button_3.setBounds(629, 32, 113, 19);
+		contentPane.add(button_3);
+		
+		JButton button_4 = new JButton("ログアウト");
+		button_4.setBounds(754, 32, 128, 19);
+		contentPane.add(button_4);
+	}
 }
