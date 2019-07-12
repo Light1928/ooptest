95a98af8569097f4a01c4f9fe63bae4ca8e681f6
Light <51193957+Light1928@users.noreply.github.com>
2019-07-12 10:43:18 +0900

    画面レイアウトの変更

--- ooptest/Meisai.java
+++ ooptest/Meisai.java
@@ -25,10 +25,10 @@ public class Meisai extends JFrame {
 
 
 	public Meisai() {
+		setSize(900,600);
 		setLocationRelativeTo(null);
 		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 700, 490);
 		contentPane = new JPanel();
 		contentPane.setBackground(new Color(255,255,255));
 		contentPane.setLayout(null);
@@ -37,64 +37,64 @@ public class Meisai extends JFrame {
 
 		JLabel lblSalapay = new JLabel("SalaPay");
 		lblSalapay.setHorizontalAlignment(SwingConstants.CENTER);
-		lblSalapay.setBounds(181, 13, 322, 75);
+		lblSalapay.setBounds(266, 33, 322, 75);
 		lblSalapay.setFont(new Font("MS ゴシック",Font.BOLD,80));
 		contentPane.add(lblSalapay);
 
 		JLabel label = new JLabel("名前：");
 		label.setHorizontalAlignment(SwingConstants.CENTER);
 		label.setFont(new Font("MS UI Gothic", Font.PLAIN, 21));
-		label.setBounds(181, 115, 312, 29);
+		label.setBounds(286, 169, 312, 29);
 		contentPane.add(label);
 
 		JLabel lblNewLabel = new JLabel("基本時給：");
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel.setBounds(103, 168, 134, 29);
+		lblNewLabel.setBounds(208, 222, 134, 29);
 		contentPane.add(lblNewLabel);
 
 		JLabel label_1 = new JLabel("深夜時給：");
 		label_1.setHorizontalAlignment(SwingConstants.CENTER);
-		label_1.setBounds(266, 168, 134, 29);
+		label_1.setBounds(371, 222, 134, 29);
 		contentPane.add(label_1);
 
 		JLabel label_2 = new JLabel("残業時給：");
 		label_2.setHorizontalAlignment(SwingConstants.CENTER);
-		label_2.setBounds(444, 168, 134, 29);
+		label_2.setBounds(549, 222, 134, 29);
 		contentPane.add(label_2);
 
 		JLabel label_3 = new JLabel("基本時間：");
 		label_3.setHorizontalAlignment(SwingConstants.CENTER);
-		label_3.setBounds(103, 234, 134, 29);
+		label_3.setBounds(208, 288, 134, 29);
 		contentPane.add(label_3);
 
 		JLabel label_4 = new JLabel("深夜時間：");
 		label_4.setHorizontalAlignment(SwingConstants.CENTER);
-		label_4.setBounds(266, 234, 134, 29);
+		label_4.setBounds(371, 288, 134, 29);
 		contentPane.add(label_4);
 
 		JLabel label_5 = new JLabel("残業時間：");
 		label_5.setHorizontalAlignment(SwingConstants.CENTER);
-		label_5.setBounds(444, 234, 134, 29);
+		label_5.setBounds(549, 288, 134, 29);
 		contentPane.add(label_5);
 
 		JLabel label_6 = new JLabel("基本給料：");
 		label_6.setHorizontalAlignment(SwingConstants.CENTER);
-		label_6.setBounds(103, 302, 134, 29);
+		label_6.setBounds(208, 356, 134, 29);
 		contentPane.add(label_6);
 
 		JLabel label_7 = new JLabel("深夜給料：");
 		label_7.setHorizontalAlignment(SwingConstants.CENTER);
-		label_7.setBounds(266, 302, 134, 29);
+		label_7.setBounds(371, 356, 134, 29);
 		contentPane.add(label_7);
 
 		JLabel label_8 = new JLabel("残業給料：");
 		label_8.setHorizontalAlignment(SwingConstants.CENTER);
-		label_8.setBounds(444, 302, 134, 29);
+		label_8.setBounds(549, 356, 134, 29);
 		contentPane.add(label_8);
 
 		JLabel label_9 = new JLabel("総支給額：");
 		label_9.setHorizontalAlignment(SwingConstants.CENTER);
-		label_9.setBounds(103, 359, 317, 50);
+		label_9.setBounds(208, 413, 317, 50);
 		contentPane.add(label_9);
 
 		//サブボタン作成  新規・削除・月選択・ログアウト
@@ -189,7 +189,7 @@ public class Meisai extends JFrame {
 		);
 
 		JButton btnNewButton = new JButton("Back");
-		btnNewButton.setBounds(444, 372, 101, 25);
+		btnNewButton.setBounds(549, 426, 101, 25);
 		contentPane.add(btnNewButton);
 		
 		
