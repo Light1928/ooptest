3844e12dc9a1575598e4ecdee8c7389a822629c5
Light <51193957+Light1928@users.noreply.github.com>
2019-07-17 10:32:32 +0900

    背景の色を変えたよ

--- ooptest/Data_input.java
+++ ooptest/Data_input.java
@@ -31,46 +31,47 @@ public class Data_input extends JFrame {
 		setResizable(false);
 		contentPane = new JPanel();
 	//	contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setBackground(new Color(255,255,255));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 
 		dataIn = new JLabel("データ入力");
 		dataIn.setFont(new Font("MS UI Gothic", Font.PLAIN, 50));
-		dataIn.setBounds(352, 38, 274, 63);
+		dataIn.setBounds(438, 82, 231, 64);
 		contentPane.add(dataIn);
 
 	 normal = new JLabel("通常時給");
 		normal.setFont(new Font("MS UI Gothic", Font.PLAIN, 20));
-		normal.setBounds(276, 244, 97, 29);
+		normal.setBounds(348, 283, 97, 29);
 		contentPane.add(normal);
 
 		 normaltime = new JLabel("勤務時間");
 		normaltime.setFont(new Font("MS UI Gothic", Font.PLAIN, 20));
-		normaltime.setBounds(276, 316, 81, 27);
+		normaltime.setBounds(348, 349, 81, 27);
 		contentPane.add(normaltime);
 
 		JLabel label_3 = new JLabel("労働時間");
 		label_3.setFont(new Font("MS UI Gothic", Font.PLAIN, 20));
-		label_3.setBounds(276, 384, 87, 26);
+		label_3.setBounds(348, 414, 87, 26);
 		contentPane.add(label_3);
 
 		JLabel label_4 = new JLabel("年月日");
 		label_4.setFont(new Font("MS UI Gothic", Font.PLAIN, 20));
-		label_4.setBounds(276, 174, 81, 22);
+		label_4.setBounds(348, 204, 81, 22);
 		contentPane.add(label_4);
 
 		normal_money = new JTextField();
-		normal_money.setBounds(419, 247, 123, 28);
+		normal_money.setBounds(562, 286, 123, 28);
 		contentPane.add(normal_money);
 		normal_money.setColumns(10);
 
 		night_money = new JTextField();
-		night_money.setBounds(419, 318, 123, 29);
+		night_money.setBounds(562, 351, 123, 29);
 		contentPane.add(night_money);
 		night_money.setColumns(10);
 
 		over_money = new JTextField();
-		over_money.setBounds(419, 386, 123, 29);
+		over_money.setBounds(562, 428, 123, 29);
 		contentPane.add(over_money);
 		over_money.setColumns(10);
 
@@ -89,40 +90,40 @@ public class Data_input extends JFrame {
 		contentPane.add(month);
 
 		day = new JComboBox();
-		day.setBounds(573, 174, 46, 29);
+		day.setBounds(686, 204, 46, 29);
 		day.setModel(new DefaultComboBoxModel(new String[] {"1","2","3","4","5","6","7","8","9","10","11","12",
 				"13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31",}));
 		contentPane.add(day);
 
 
 
-		year.setBounds(369, 175, 84, 27);
+		year.setBounds(429, 205, 84, 27);
 
 
-		month.setBounds(483, 174, 52, 28);
+		month.setBounds(583, 204, 52, 28);
 
 
 
 		JLabel label_5 = new JLabel("年");
-		label_5.setBounds(454, 180, 17, 16);
+		label_5.setBounds(525, 210, 17, 16);
 		contentPane.add(label_5);
 
 		JLabel label_6 = new JLabel("月");
-		label_6.setBounds(539, 180, 22, 16);
+		label_6.setBounds(647, 216, 22, 16);
 		contentPane.add(label_6);
 
 		JLabel label_7 = new JLabel("日");
-		label_7.setBounds(621, 179, 22, 19);
+		label_7.setBounds(740, 215, 22, 19);
 		contentPane.add(label_7);
 
 
 		 over = new JLabel("残業時間");
 		over.setFont(new Font("MS UI Gothic", Font.PLAIN, 20));
-		over.setBounds(276, 449, 81, 28);
+		over.setBounds(354, 487, 81, 28);
 		contentPane.add(over);
 
 		JTextField normal_money_3 = new JTextField();
-		normal_money_3.setBounds(419, 452, 123, 29);
+		normal_money_3.setBounds(562, 490, 123, 29);
 		contentPane.add(normal_money_3);
 		normal_money_3.setColumns(10);
 
@@ -133,7 +134,7 @@ public class Data_input extends JFrame {
 		regist = new JButton("登録");
 		regist.setForeground(Color.BLACK);
 		regist.setFont(new Font("Helvetica Neue",Font.BOLD,30));
-		regist.setBounds(528,559,100,30);
+		regist.setBounds(487,585,100,30);
 		regist.setActionCommand("登録");
 		contentPane.add(regist);
 /*		regist.addActionListener(new ActionListener() {
